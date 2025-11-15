8e4493c83d319980080684f486d29742265d5c21
IEVN1003-22002257 <jonymendez40@gmail.com>
2025-11-14 18:38:54 -0600

    Api completo

--- src/app.py
+++ src/app.py
@@ -1,14 +1,17 @@
+# app.py
+
 from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL  
 from flask_cors import CORS
 from config import config 
 
 app = Flask(__name__)
-CORS(app)  
+
+
+CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5000"}}) 
 
 
 app.config.from_object(config['development'])
-
 conexion = MySQL(app)
 
 @app.route('/alumnos', methods=['GET'])
@@ -27,11 +30,10 @@ def listar_alumnos():
                 'matricula': fila[0],
                 'nombre': fila[1],
                 'apellido': apellido_completo, 
-                'correo': fila[4]         
+                'correo': fila[4]
             }
             alumnos.append(alumno)
             
-
         return jsonify({'alumnos': alumnos, 'mensaje': 'Alumnos encontrados', "exito": True})
     
     except Exception as ex:
@@ -46,9 +48,8 @@ def pagina_no_encontrada(error):
 def leer_alumno_bd(matricula):
     try:
         cursor = conexion.connection.cursor()
-
         sql = "SELECT matricula, nombre, apaterno, amaterno, correo FROM alumnos WHERE matricula = %s"
-        cursor.execute(sql, (matricula,)) # Pasamos la matrícula como tupla
+        cursor.execute(sql, (matricula,))
         datos = cursor.fetchone()
         if datos != None:
             alumno = {'matricula': datos[0], 'nombre': datos[1], 'apaterno': datos[2], 'amaterno': datos[3], 'correo': datos[4]}
@@ -59,7 +60,7 @@ def leer_alumno_bd(matricula):
         raise ex
 
 @app.route('/alumnos/<mat>', methods=['GET'])
-def leer_curso(mat):  
+def leer_curso(mat): 
     try:
         alumno = leer_alumno_bd(mat) 
         if alumno != None:
@@ -68,6 +69,59 @@ def leer_curso(mat):
             return jsonify({'mensaje': "Alumno no encontrado.", 'exito': False})
     except Exception as ex:
         return jsonify({'mensaje': f"Error: {ex}", 'exito': False})
+
+
+@app.route("/alumnos",methods=['POST'])
+def registrar_alumno():
+    try:
+        alumno=leer_alumno_bd(request.json['matricula'])
+        if alumno != None:
+            return jsonify({'mensaje':"Alumno ya existe, no se puede duplicar",
+                             'exito':False})
+        else:
+            cursor=conexion.connection.cursor()
+            sql="""insert into alumnos (matricula,nombre,apaterno,amaterno,correo)
+             values ('{0}','{1}','{2}','{3}','{4}')""".format(request.json['matricula'],
+                 request.json['nombre'],request.json['apaterno'],request.json['amaterno'],
+                 request.json['correo'])
+            cursor.execute(sql)
+            conexion.connection.commit()
+            return jsonify({'mensaje':"Alumno registrado","exito":True})
+        
+    except Exception as ex:
+        return jsonify({'mensaje': "Error", 'exito': False})
+    
+@app.route('/alumnos/<mat>', methods=['PUT'])
+def actualizar_curso(mat):
+        try:
+            alumno = leer_alumno_bd(mat)
+            if alumno != None:
+                cursor = conexion.connection.cursor()
+                sql = """UPDATE alumnos SET nombre = '{0}', apaterno = '{1}', amaterno='{2}', correo='{3}'
+                WHERE matricula = {4}""".format(request.json['nombre'], request.json['apaterno'], request.json['amaterno'],request.json['correo'], mat)
+                cursor.execute(sql)
+                conexion.connection.commit()  # Confirma la acción de actualización.
+                return jsonify({'mensaje': "Alumno actualizado.", 'exito': True})
+            else:
+                return jsonify({'mensaje': "Alumno no encontrado.", 'exito': False})
+        except Exception as ex:
+            return jsonify({'mensaje': "Error {0} ".format(ex), 'exito': False})
+        
+@app.route('/alumnos/<mat>', methods=['DELETE'])
+def eliminar_curso(mat):
+    try:
+        alumno = leer_alumno_bd(mat)
+        if alumno != None:
+            cursor = conexion.connection.cursor()
+            sql = "DELETE FROM alumnos WHERE matricula = {0}".format(mat)
+            cursor.execute(sql)
+            conexion.connection.commit()  # Confirma la acción de eliminación.
+            return jsonify({'mensaje': "Alumno eliminado.", 'exito': True})
+        else:
+            return jsonify({'mensaje': "Alumno no encontrado.", 'exito': False})
+    except Exception as ex:
+        return jsonify({'mensaje': "Error", 'exito': False})
+
 
 
 if __name__ == '__main__':
